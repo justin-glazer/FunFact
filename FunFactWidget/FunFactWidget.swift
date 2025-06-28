c803359168f47184c90b3fb2e94398ff206655d9
Justin <jglazer365@gmail.com>
2025-06-28 00:38:42 -0400

    Finished basic date structure and added simple styling

--- FunFactWidget/FunFactWidget.swift
+++ FunFactWidget/FunFactWidget.swift
@@ -9,23 +9,24 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+    func placeholder(in context: Context) -> DayEntry {
+        DayEntry(date: Date())
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
+    func getSnapshot(in context: Context, completion: @escaping (DayEntry) -> ()) {
+        let entry = DayEntry(date: Date())
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+        var entries: [DayEntry] = []
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        // Generate a timeline consisting of seven entries a day apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
+        for dayOffset in 0 ..< 7 {
+            let entryDate = Calendar.current.date(byAdding: .day, value: dayOffset, to: currentDate)!
+            let startOfDate = Calendar.current.startOfDay(for: entryDate)
+            let entry = DayEntry(date: startOfDate)
             entries.append(entry)
         }
 
@@ -34,15 +35,34 @@ struct Provider: TimelineProvider {
     }
 }
 
-struct SimpleEntry: TimelineEntry {
+struct DayEntry: TimelineEntry {
     let date: Date
 }
 
 struct FunFactWidgetEntryView : View {
-    var entry: Provider.Entry
+    var entry: DayEntry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        ZStack{
+            ContainerRelativeShape()
+                .fill(.blue.gradient)
+            VStack{
+                HStack{
+                    Text(entry.date.formatted(.dateTime.weekday(.wide)))
+                        .font(.title)
+                    Text(entry.date.formatted(.dateTime.month(.wide)))
+                        .font(.title)
+                    Text(entry.date.formatted(.dateTime.day()))
+                        .font(.title)
+                    
+                }
+                Text("Fact Here")
+                    .font(.headline)
+            
+            }
+            
+        }
+        
     }
 }
 
@@ -53,14 +73,15 @@ struct FunFactWidget: Widget {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             FunFactWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Fun Fact Widget")
+        .description("Gives a fun fact based on the day.")
+        .supportedFamilies([.systemMedium])
     }
 }
 
 struct FunFactWidget_Previews: PreviewProvider {
     static var previews: some View {
-        FunFactWidgetEntryView(entry: SimpleEntry(date: Date()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+        FunFactWidgetEntryView(entry: DayEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
     }
 }
