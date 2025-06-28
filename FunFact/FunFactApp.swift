5826685a1124635c287cda56ebc7f411134fe564
Justin <jglazer365@gmail.com>
2025-06-27 21:03:27 -0400

    Initial Commit

---
+++ FunFact/FunFactApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunFactApp.swift
+//  FunFact
+//
+//  Created by Justin Glazer on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct FunFactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
