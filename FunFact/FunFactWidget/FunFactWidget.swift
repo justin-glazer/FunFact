1d2df96ea8426561abb3a2b6a63d64ac1ea32064
Justin <jglazer365@gmail.com>
2025-06-30 00:50:26 -0400

    edited style of widget:

--- FunFact/FunFactWidget/FunFactWidget.swift
+++ FunFact/FunFactWidget/FunFactWidget.swift
@@ -10,23 +10,24 @@ import SwiftUI
 import Intents
 
 struct Provider: IntentTimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+    func placeholder(in context: Context) -> DayEntry {
+        DayEntry(date: Date(), configuration: ConfigurationIntent())
     }
 
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (DayEntry) -> ()) {
+        let entry = DayEntry(date: Date(), configuration: configuration)
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+        var entries: [DayEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+        for dayOffset in 0 ..< 7 {
+            let entryDate = Calendar.current.date(byAdding: .day, value: dayOffset, to: currentDate)!
+            let startOfDate = Calendar.current.startOfDay(for: entryDate)
+            let entry = DayEntry(date: startOfDate, configuration: configuration)
             entries.append(entry)
         }
 
@@ -35,7 +36,7 @@ struct Provider: IntentTimelineProvider {
     }
 }
 
-struct SimpleEntry: TimelineEntry {
+struct DayEntry: TimelineEntry {
     let date: Date
     let configuration: ConfigurationIntent
 }
@@ -44,7 +45,26 @@ struct FunFactWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        ZStack{
+            ContainerRelativeShape()
+                .fill(.green.gradient)
+            VStack{
+                HStack{
+                    Text(entry.date.formatted(.dateTime.weekday(.wide)))
+                        .font(.title)
+                    Text(entry.date.formatted(.dateTime.month(.wide)))
+                        .font(.title)
+                    Text(entry.date.formatted(.dateTime.day()))
+                        .font(.title)
+                    
+                }
+                Text("Fact Goes Here")
+                    .font(.headline)
+            
+            }
+            
+        }
+        
     }
 }
 
@@ -55,14 +75,15 @@ struct FunFactWidget: Widget {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             FunFactWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Fun Fact Widget")
+        .description("Gives a fun fact based on the day.")
+        .supportedFamilies([.systemMedium])
     }
 }
 
 struct FunFactWidget_Previews: PreviewProvider {
     static var previews: some View {
-        FunFactWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+        FunFactWidgetEntryView(entry: DayEntry(date: Date(), configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
     }
 }
