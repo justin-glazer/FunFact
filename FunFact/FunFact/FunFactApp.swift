3f108f9e74babc5d489bc2c27acc730f0cb8adcd
Justin <jglazer365@gmail.com>
2025-06-30 00:32:27 -0400

    created widget

---
+++ FunFact/FunFact/FunFactApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunFactApp.swift
+//  FunFact
+//
+//  Created by Justin Glazer on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct FunFactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FunFact/FunFactWidget/FunFactWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  FunFactWidgetBundle.swift
+//  FunFactWidget
+//
+//  Created by Justin Glazer on 6/30/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct FunFactWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        FunFactWidget()
+    }
+}
